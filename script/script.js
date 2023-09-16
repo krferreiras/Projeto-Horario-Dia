bb82fbfd729cbaccebff21a81734babb6bda2755
Kaio Herrera <krsantos.login@hotmail.com>
2023-09-16 20:30:13 -0300

    alteração nos links, estava dando bug ao chamar o link

--- script/script.js
+++ script/script.js
@@ -6,13 +6,13 @@ function carregar() {
     const minutes = data.getMinutes()
     msg.innerHTML = `Agora são ${hora}:${minutes}`
     if (hora >= 6 && hora < 13) {
-        img.src = '../fotos/manha.png'
+        img.src = 'fotos/manha.png'
         document.body.style.background = 'radial-gradient(circle at top,rgb(166, 220, 243)45%, rgb(138, 196, 220)60%, rgb(54, 78, 88))'
     } else if (hora >= 12 && hora <= 18) {
-        img.src = '../fotos/tarde.png'
+        img.src = 'fotos/tarde.png'
         document.body.style.background = 'radial-gradient(circle at top,rgb(253, 206, 68)45%, rgb(253, 179, 68)60%, rgb(71, 41, 6))'
     } else {
-        img.src = '../fotos/noite.png'
+        img.src = 'fotos/noite.png'
         document.body.style.background = 'radial-gradient(circle at top, rgb(75, 89, 116)45%, rgb(31, 40, 57)70%, black)'
     }
 }
